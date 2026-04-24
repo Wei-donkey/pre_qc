b5a1aea54924f015d1be61b85dc6fdab8860f377
liuwei <6822762@qq.com>
2026-04-24 08:46:25 +0800

    Rename script to fetch_precip_awst_samples and update logic

--- src/fetch_precip_awst_samples.py
+++ src/fetch_precip_awst_samples.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu April 13 20:15:51 2026
-Query two datasets and combine they before output: 
-1. Hourly precipitation over 25mm from national observatories, all of which are absolutely correct thanks to manual inspection;
-2. Hourly precipitation over 184.4mm from automatic weather stations, which are absolutely incorrect beyond climate limitations.
+Query two datasets from automatic weather stations: 
+1. Hourly precipitation over 184.4mm which are absolutely incorrect beyond climate limitations.
+2. Hourly precipitation between 50~184.4mm which are to be inspected for their reliability.
+The two datasets are merged together and sorted by time and station code.
 Concatenate them into one file and ascend all records in time order.
 @author: Wei Liu
 """
@@ -34,8 +35,8 @@ DB_CONFIG = {
     'service': config['CROSS_WEATHER']['service']
 }
 
-datatypes = ['true','false','uncertain']
-statypes = ['surf','awst','awst']
+datatypes = ['false','uncertain']
+statypes = ['awst','awst']
 
 year_stt, year_end = 2003, 2025
 years = range(year_stt, year_end+1)
@@ -53,6 +54,8 @@ def main():
     try:
         user_db = DB_CONFIG['username']
         pwd_db = DB_CONFIG['password']
+        # Encode password for URL
+        pwd_db = quote(pwd_db)
         host_db = DB_CONFIG['host']
         port_db = DB_CONFIG['port']
         service_db = DB_CONFIG['service']
@@ -62,9 +65,6 @@ def main():
     except KeyError as e:
         print(f"Missing key in DB_CONFIG: {e}")
         return None
-
-    # Encode password for URL
-    pwd_db = quote(pwd_db)
     
     engine = None    
     try:
@@ -80,16 +80,14 @@ def main():
             
             for datatype in datatypes:
                 statype = statypes[datatypes.index(datatype)]
-                data_tb_year = f'{statype}_{DATA_TB}_{year}'
+                data_tb_year = f"{statype}_{DATA_TB}_{year}"
                             
                 base_sql = (
                     f"select stacode,ddatetime,r "
                     f"from {data_tb_year} "
                     f"where length(stacode)=5" 
                 )
-                if datatype == 'true':
-                    sql = base_sql + " and r >= 50"
-                elif datatype == 'false':
+                if datatype == 'false':
                     sql = base_sql + " and r > 184.4"
                 elif datatype == 'uncertain':
                     sql = base_sql + " and r <= 184.4 and r >= 50"
@@ -101,10 +99,8 @@ def main():
                     print(f"no {datatype} data for {year}")
                     continue
                 else:
-                    if datatype == 'true': 
-                        df['statype'], df['datatype'] = 'surf', 1
-                    elif datatype == 'false':
-                        df['statype'], df['datatype'] = 'awst', 0
+                    if datatype == 'false':
+                        df['statype'], df['datatype'] = 'awst', 1
                     elif datatype == 'uncertain':
                         df['statype'], df['datatype'] = 'awst', 9
                     year_data.append(df)
